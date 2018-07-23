39c625a5de490fce49a7aa840364486759de09a0
memolop <inglopezvargas@gmail.com>
2018-07-23 14:55:02 -0500

    heroes

---
+++ Documentos/pmemo/proyectomemo/src/app/heroes-detalle/heroes-detalle.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-heroes-detalle',
+  templateUrl: './heroes-detalle.component.html',
+  styleUrls: ['./heroes-detalle.component.css']
+})
+export class HeroesDetalleComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
